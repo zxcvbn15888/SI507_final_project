c8c460fabf7cfd93f163a676468c8e6bca4b34e7
zxcvbn15888 <58970001+zxcvbn15888@users.noreply.github.com>
2023-04-10 22:15:04 -0400

    Add files via upload

---
+++ data_structure.py
@@ -0,0 +1,10 @@
+tree = ['released years of the movie: ', 
+         ['genre of the movie:', 
+           ['runtime of the movie:'], 
+         ], 
+         ['some other info of the movie:',
+           [], 
+           [], 
+           [] 
+         ] 
+       ]
